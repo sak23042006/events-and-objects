fa24c9258fd82ebcc9a08c22799b3b6fcca74455
sak23042006 <s.arunkumar567567567@gmail.com>
2023-10-18 18:25:54 +0530

    commit

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 //Challenge 1: Given an array of strings, write a code to return a new array that only includes the strings that are 5 characters or more in length using filter method.
 const strings = ['dog', 'by', 'cat', 'camping', 'large', 'flower', 'rabbit'];
+console.log(strings.filter(str));
+function str(string) {
+  if(string.length>=5){
+    return string
+  }
+}
 
 //Challenge 2: Given an array of numbers, write a code to return a new array of squares for each item in an array using map method.
 const numbers = ['45', '55', '65', '75', '85'];
 
+console.log(numbers.map((sqr)=>{
+  
+  return sqr**2
+
+}))
+
 //Challenge 3: Given an array of alphabets, write a code to concatenate all the elements of the array "alphabets" to make it as a string and print the same in console. Hint: Use reduce method.
 const alphabets = ['K', 'a', 'l', 'v', 'i', 'u', 'm'];
+
+console.log(alphabets.reduce((alphabets,letter)=>{
+
+  return alphabets += letter
+
+}));
 
 //Challenge 4: Given an array of numbers, write a code to return a first even number using find method.
 const number = [1, 2, 3, 4, 5];
 
+console.log(number.find((firstEven)=>{
+
+  if(firstEven%2==0){
+    return firstEven
+  }
+
+}));
+
 //Challenge 5: Given a list of ranks, write a code to return the index of the first occurrence of the number 7 in the ranks array using findIndex method.
 const ranks = [1, 5, 7, 8, 10, 7];
+
+console.log(ranks.findIndex(element => element==7));
 
 //Challenge 6: Given a list of numbers, using forEach loop iterate over each element of the array to print the output as:
 // 0:2
@@ -20,11 +48,25 @@ const ranks = [1, 5, 7, 8, 10, 7];
 //3:8
 const num = [2, 4, 6, 8];
 
+num.forEach((n,index)=>{
+
+  console.log(`${index} : ${n}`);
+
+})
+
 //Challenge 7: Given an array of marks, using "some method", check if the following array has at least one element less than 45 and print true.
 const marks = [35, 66, 25, 85, 75, 45];
 
+console.log(marks.some(n =>{
+
+  return n < 45
+
+}));
+
 //Challenge 8: Given an array of numbers, write a code to check if all the array elements are positive by using "every method" and print true if all the numbers are positive, else print false.
 const positive = [2, 3, 4, -1];
+
+console.log(positive.every(n => n > 0));
 
 //Challenge 9: Given an array of objects, using dot and bracket notation print the output as :
 //1. "Audi"
@@ -54,6 +96,12 @@ const cars = [
   },
 ];
 
+console.log(cars[0].name);
+console.log(cars[2].name,cars[2].color[0]);
+console.log(cars[1].name,cars[1].color[2],cars[1].capacity);
+console.log(cars[3].name,cars[3].color[1],cars[3].capacity);
+
+
 //Challenge 10: Given a nested object,
 //1. Delete the "phoneno" property from the object person.
 //2. Using the hasOwnProperty check whether the person object has the property "bike", "house".
@@ -72,3 +120,19 @@ const person = {
   email: 'ajay@gmail.com',
   phoneno: 9978425555,
 };
+
+// 1
+delete person.phoneno ;
+
+// 2
+console.log(person.hasOwnProperty("bike"));
+console.log(person.hasOwnProperty("house"));
+
+// 3
+console.log(`Mr ${person.name} travels by plane called by ${person.vehicles.plane}`);
+
+// 4
+person.email="ajaykumar@gmail.com"
+
+// 5
+console.log(Object.keys(person));
